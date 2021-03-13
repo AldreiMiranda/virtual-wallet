d24908dea677f02af55aecbcae736b0aeddd3896
Aldrei Miranda <aldrei_miranda@hotmail.com>
2021-03-13 20:10:13 -0300

    create login screen (#2)

--- src/containers/newLogin.js
+++ src/containers/newLogin.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import {
+  TextField,
+  Grid,
+  Button
+} from '@material-ui/core'
+import './../assets/css/login.css'
 
-function NewLogin(){
-    return(
-        <h1>Novo login aqui </h1>
-    );
+function Login() {
+  return (
+    <div className="Login" style={{ marginTop: 200 }}>
+      <Grid container spacing={1} >
+        <Grid item xs={12}>
+          <p> Criar uma nova conta </p>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            id="outlined-password-input"
+            label="Usuario"
+            type="password"
+            autoComplete="current-password"
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            id="outlined-password-input"
+            label="Senha"
+            type="password"
+            autoComplete="current-password"
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            id="outlined-password-input"
+            label="Confirmar senha"
+            type="password"
+            autoComplete="current-password"
+            variant="outlined"
+          />
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <Button style={{ marginTop: 5 }} variant="contained" color="primary">
+          criar
+      </Button>
+      </Grid>
+
+
+    </div>
+  );
 };
 
-export default NewLogin;
+export default Login;
